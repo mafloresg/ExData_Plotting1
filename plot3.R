92b236141253a189284a5324ae074dd293f31e8b
Angeles F <m.angeles.flores@gmail.com>
2015-02-08 21:47:00 +0100

    Plot3
    R code and png image.

---
+++ plot3.R
@@ -0,0 +1,28 @@
+## Plot2: timeline
+
+## Download and unzip the data file
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="exdata_data_household_power_consumption.zip", method="curl")
+untar('exdata_data_household_power_consumption.zip')
+
+
+## Load the file and subset the data for the two days asked
+## The same as plot1.R
+
+hpc <- read.table("household_power_consumption.txt", sep=";",header=TRUE, dec=".", fill=TRUE)
+shpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007"),]
+
+
+## Create a new datetime column and print the plot
+
+shpc$datetime <- as.POSIXlt(paste(as.Date(shpc$Date,format="%d/%m/%Y"), shpc$Time, sep=" "))
+plot(shpc$datetime, shpc$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+with(shpc, lines(datetime,Sub_metering_2, col="red"))
+with(shpc, lines(datetime,Sub_metering_3, col="blue"))
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+## Export the plot3 in a png file
+
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
